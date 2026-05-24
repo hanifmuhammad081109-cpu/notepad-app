4a3e09a285bf3d5c6d7f4310d9c0a08ccd37d595
hanifmuhammad081109-cpu <hanifmuhammad081109@gmail.com>
2026-05-24 14:56:42 +0300

    vite update login register

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -2,10 +2,34 @@
 
 namespace App\Livewire\Auth;
 
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+
+#[Layout('components.layouts.app')]
+#[Title('Login - Sistem Payroll')]
 
 class Login extends Component
 {
+    public $email = '';
+    public $password = '';
+
+    public function authenticate()
+    {
+        //Validasi input
+        $credentials = $this->validate([
+            'email' => 'required|email',
+            'password' => 'required|',
+        ]);
+
+        //Coba Login
+        if (Auth::attempt($credentials)) {
+            session()->regenerate();
+            return redirect()->intended('/');
+        }
+    }
+
     public function render()
     {
         return view('livewire.auth.login');
--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -2,12 +2,46 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Register extends Component
 {
+    public $name = '';
+    public $email = '';
+    public $password = '';
+    public $password_confirmation = '';
+
+    public function register()
+    {
+        // 1. Validasi Input
+        $this->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        // 2. Simpan User ke Database
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
+
+        // 3. Login otomatis setelah daftar
+        event(new Registered($user));
+        Auth::login($user);
+
+        // 4. Redirect ke dashboard
+        return redirect()->route('admin.dashboard');
+    }
+
     public function render()
     {
-        return view('livewire.auth.register');
+        return view('livewire.auth.register')
+               ->layout('layouts.guest');
     }
 }
