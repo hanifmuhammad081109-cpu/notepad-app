c61c6c32b55474974c11d05da5cd1542f1d0a891
hanifmuhammad081109-cpu <hanifmuhammad081109@gmail.com>
2026-05-24 10:58:25 +0300

    logic update

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
 {
-    //
+    protected $fillable = ['user_id', 'title', 'content'];
+
+    // Relasi dengan User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
